6246f65b55a1f987b45163005871e2cbe164926b
diekai <1979837819@qq.com>
2012-03-25 18:52:46 -0700

    Update FrontEnd/AppDelegate.h

--- FrontEnd/AppDelegate.h
+++ FrontEnd/AppDelegate.h
@@ -7,11 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+@class FirstViewController;
+@class SecondViewController;
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>{
+    FirstViewController  *viewController1;
+    SecondViewController *viewController2 ;
+}
+
 
 @property (strong, nonatomic) UIWindow *window;
 
 @property (strong, nonatomic) UITabBarController *tabBarController;
+@property (retain, nonatomic) FirstViewController  *viewController1;
+@property (retain, nonatomic) SecondViewController  *viewController2;
 
 @end
